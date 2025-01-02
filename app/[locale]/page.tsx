a5a47a158c3216fd4250262165f64f40a3f72176
Donetta Flatley <603476106@qq.com>
2025-01-03 00:08:31 +0800

    更新代码

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,8 +3,30 @@ import Logo from "@/assest/images/logo.png";
 import { useTranslations } from "next-intl";
 import HeaderMenuBtn from "./menuBtn";
 import { Link } from "@/i18n/routing";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import HeaderContainer from "./headerContainer";
+
+function HomePage() {
+  const t = useTranslations("homePage");
+
+  return  <main className="pt-28 flex flex-col">
+    <div className="text-center">
+      <h1 className="text-xl font-medium">
+      {t.rich("title", {
+        model: (chunks) => <span className="text-purple-500">{ chunks}</span>
+      })}
+      </h1>
+      
+      <p className="mt-10">PDF, DOCX, ePub...</p>
+      <p className="my-1">{ t('layout')}</p>
+      <p className="mb-8">{t('translated')}</p>
+      
+      <Button className="w-7/12">
+        {t("upload")}
+      </Button>
+        </div>
+      </main>
+}
 
 export default function Home() {
   const t = useTranslations("header");
@@ -25,7 +47,7 @@ export default function Home() {
         </Link>
       </HeaderContainer>
 
-      <div className="pt-28 flex flex-col"></div>
+     <HomePage/>
     </>
   );
 }
